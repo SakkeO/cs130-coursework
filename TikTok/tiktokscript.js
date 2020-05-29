68e72b76887a9b471ba265b2b83936990c3ed954
SakkeO <sakkeoverlund2021@u.northwestern.edu>
2020-05-28 21:58:29 -0700

    second deliverable

---
+++ TikTok/tiktokscript.js
@@ -0,0 +1,13 @@
+$(document).scroll(function(){
+   if ($(document).scrollTop() > 320 ) {
+	   $('.main').addClass('active');
+	   $('.reveal-main').addClass('activetwo');
+   }
+	
+	else {
+		$('.main').removeClass('active');
+         $('.reveal-main').removeClass('activetwo');
+	}
+
+});
+ 
